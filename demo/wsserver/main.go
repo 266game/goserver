3c8e89f4a5691fcb6e1c4c95da9fc97edc451e86
warrially <warrially@139.com>
2020-07-02 16:36:28 +0800

    demo

---
+++ demo/wsserver/main.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/266game/goserver/conn"
+	"github.com/266game/goserver/ws"
+)
+
+func main() {
+	pServer := &ws.TWSServer{}
+
+	pServer.Start(":4567")
+
+	pServer.OnRead = func(pData *conn.TData) {
+		pData.Print()
+	}
+
+	pServer.OnRun = func(pConnection *conn.TConnection) {
+		buf := make([]byte, 4096)
+		for {
+			nLen, err := pConnection.Read(buf)
+			if err != nil {
+				return
+			}
+			log.Println("实际接收的包长", nLen, err)
+			log.Println(string(buf[:nLen]))
+		}
+	}
+
+	time.Sleep(time.Second * 100)
+}
